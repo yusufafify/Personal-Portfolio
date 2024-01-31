1853973f4db6a8a13f375b10d74593da8484ff4b
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-01-31 19:51:39 +0200

    changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -75,14 +75,26 @@ const ProjectCard = ({ index, project }) => {
 const Works = () => {
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      <motion.div >
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively.
+        </motion.p>
+      </div>
       
       <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
-          <motion.div key={`project-${index}`} variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+          <motion.div key={`project-${index}`} >
 
           <ProjectCard
             
