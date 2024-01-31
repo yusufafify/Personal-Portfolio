03f0c70b88cb9bd039c4e580771cb863b6be3358
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-01-31 19:43:10 +0200

    changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -7,39 +7,30 @@ import { github, preview } from "../assets";
 import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 
-const ProjectCard = ({
-  index,
-  name,
-  description,
-  tags,
-  image,
-  source_code_link,
-  live_preview_link,
-}) => {
+const ProjectCard = ({ index, project }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
       <Tilt
         options={{
           max: 45,
           scale: 1,
           speed: 450,
         }}
-        className='bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full'
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
       >
         <div className="relative w-full h-[230px]">
           <img
-            src={image}
-            alt={name}
+            src={project.image}
+            alt={project.name}
             className="w-full h-full object-cover rounded-2xl"
           />
           <div
             className={`absolute inset-0 flex ${
-              live_preview_link ? "justify-between " : "justify-end"
+              project.live_preview_link ? "justify-between " : "justify-end"
             } m-3 card-img_hover`}
           >
-            {live_preview_link && (
+            {project.live_preview_link && (
               <div
-                onClick={() => window.open(live_preview_link, "_blank")}
+                onClick={() => window.open(project.live_preview_link, "_blank")}
                 className="green-pink-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
               >
                 <img
@@ -63,11 +54,13 @@ const ProjectCard = ({
           </div>
         </div>
         <div className="mt-5 ">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
+          <h3 className="text-white font-bold text-[24px]">{project.name}</h3>
+          <p className="mt-2 text-secondary text-[14px]">
+            {project.description}
+          </p>
         </div>
         <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag, index) => (
+          {project.tags.map((tag, index) => (
             <p key={index} className={`text-[14px] ${tag.color}`}>
               {" "}
               #{tag.name}
@@ -75,7 +68,7 @@ const ProjectCard = ({
           ))}
         </div>
       </Tilt>
-    </motion.div>
+
   );
 };
 
@@ -83,24 +76,27 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>My Work</p>
-        <h2 className={styles.sectionHeadText}>Projects.</h2>
-      </motion.div>
-      <div className="w-full flex">
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
-          Following projects showcases my skills and experience through
+      <p className={styles.sectionSubText}>Mt Work</p>
+        <h2 className={styles.sectionHeadText}>Project.</h2>
+        <p className={`${styles.heroSubText} mt-3 text-secondary`}>
+        Following projects showcases my skills and experience through
           real-world examples of my work. Each project is briefly described with
           links to code repositories and live demos in it. It reflects my
           ability to solve complex problems, work with different technologies,
           and manage projects effectively.
-        </motion.p>
-      </div>
+        </p>
+      </motion.div>
+      
       <div className="mt-20 flex flex-wrap gap-7">
         {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} {...project} index={index} />
+          <motion.div key={`project-${index}`} variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+
+          <ProjectCard
+            
+            project={project}
+            index={index}
+          />
+          </motion.div>
         ))}
       </div>
     </>
