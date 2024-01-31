4e7165715f967265225bfe436921266740f99cb6
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-01-31 13:21:55 +0200

    changes

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -37,7 +37,7 @@ const ProjectCard = ({
               live_preview_link ? "justify-between " : "justify-end"
             } m-3 card-img_hover`}
           >
-            {live_preview_link && (
+            
               <div
                 onClick={() => window.open(live_preview_link, "_blank")}
                 className="green-pink-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
@@ -48,7 +48,7 @@ const ProjectCard = ({
                   className="w-1/2 h-1/2 object-contain"
                 />
               </div>
-            )}
+            
 
             <div
               onClick={() => window.open(source_code_link, "_blank")}
@@ -86,9 +86,9 @@ const Works = () => {
         <p className={styles.sectionSubText}>My Work</p>
         <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      <motion.p 
+      variants={fadeIn("", "", 0.1, 1)}
+      className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         Following projects showcases my skills and experience through real-world
         examples of my work. Each project is briefly described with links to
@@ -97,12 +97,20 @@ const Works = () => {
         effectively.
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-7">
-        {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} {...project} index={index} />
-        ))}
-      </div>
+{projects.map((project, index) => (
+  <ProjectCard key={`project-${index}`} {...project} index={index} />
+))}
+</div>
     </>
   );
 };
 
 export default SectionWrapper(Works, "work");
+
+
+
+{/* <div className="mt-20 flex flex-wrap gap-7">
+{projects.map((project, index) => (
+  <ProjectCard key={`project-${index}`} {...project} index={index} />
+))}
+</div> */}
